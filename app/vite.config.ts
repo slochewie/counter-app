17d38297a4353c862ae7c607a6f3f605af33ff2f
Aaron Wilson <65009367+slochewie@users.noreply.github.com>
2026-08-29 23:32:09 -0700

    Allow Counter dev host

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -8,6 +8,9 @@ import tailwindcss from '@tailwindcss/vite'
 import { nitro } from 'nitro/vite'
 
 const config = defineConfig({
+  server: {
+    allowedHosts: ['counter.niteowl.dev'],
+  },
   resolve: { tsconfigPaths: true },
   plugins: [
     devtools(),
